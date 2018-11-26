9221cabfda46c93de978c69728a850a9997aa6ae
nicGardner <nicgardner@rogers.com>
2018-11-26 11:57:55 -0500

    added Moq to packages

---
+++ DigitalCharacterSheet2.Tests/Controllers/CharactersControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+// add reference to web project controllers
+using DigitalCharacterSheet2.Controllers;
+using DigitalCharacterSheet2.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mon_f2018.Tests.Controllers
+{
+    [TestClass]
+    public class AlbumsControllerTest
+    {
+
+    }
+}
